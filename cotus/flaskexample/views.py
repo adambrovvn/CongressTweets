f5f4e082f67f7844e78d647d868ba1ec10791bef
adambrovvn <32139775+adambrovvn@users.noreply.github.com>
2017-10-03 10:55:54 -0400

    updated app files
    cleaned views.py

--- cotus/flaskexample/views.py
+++ cotus/flaskexample/views.py
@@ -47,27 +47,3 @@ def index():
 def how():
 	return render_template('about.html')
 
-@app.route('/db')
-def birth_page():
-    sql_query = """                                                             
-                SELECT * FROM birth_data_table WHERE delivery_method='Cesarean'\
-;                                                                               
-                """
-    query_results = pd.read_sql_query(sql_query,con)
-    births = ""
-    print(query_results[:10])
-    for i in range(0,10):
-        births += query_results.iloc[i]['birth_month']
-        births += "<br>"
-    return births
-
-@app.route('/db_fancy')
-def cesareans_page_fancy():
-    sql_query = """
-               SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
-                """
-    query_results=pd.read_sql_query(sql_query,con)
-    births = []
-    for i in range(0,query_results.shape[0]):
-        births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-    return render_template('cesareans.html',births=births)
